ef1c68cb849293c8a16a424d931acd0c58ee045a
limskey <40619955+limskey@users.noreply.github.com>
2023-03-13 17:27:11 -0400

    chatter update
    update to the app

---
+++ chatgpt-app-python/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, render_template
+import requests
+
+app = Flask(__name__)
+
+# Replace <your_chatgpt_api_url> with your ChatGPT API URL
+chatgpt_api_url = "https://api.openai.com/v1/chat/completions"
+headers = { 
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer sk-'
+}
+
+data = {
+    'model': 'gpt-3.5-turbo',
+    'temperature': 0.7
+}
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    message = request.form['message']
+    messages = [{"role": "user", "content": f"{message}"}]
+    data['messages'] = messages
+    response = requests.post(chatgpt_api_url, json=data, headers=headers).json()
+    # print(response['choices'][0]['message']['content'])
+    return jsonify({'message': response['choices'][0]['message']['content']})
+
+if __name__ == '__main__':
+    app.run(debug=True)
